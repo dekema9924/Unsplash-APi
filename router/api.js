5e2400c46e0299be2fbec50b9a377c4735ce3749
Daniel Unknown <danielunknown@Daniels-MBP.attlocal.net>
2024-03-17 19:10:02 -0400

    still err

--- router/api.js
+++ router/api.js
@@ -4,6 +4,7 @@ const express = require('express');
 const routes = express.Router();
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const axios = require("axios");
 
 
 routes.use(bodyParser.urlencoded({extended: true}))
@@ -11,9 +12,11 @@ routes.use(bodyParser.urlencoded({extended: true}))
 
 routes.get('/', async(req, res)=>{
     async function getImages() {
-          const response = await fetch(`https://api.unsplash.com/search/photos?page=1&query=office&client_id=${process.env.API_KEY}`)
-          var data = await response.json()
+          const response = await axios(`https://api.unsplash.com/search/photos?page=1&query=office&client_id=${process.env.API_KEY}`)
+          var data = await response.data;
+          console.log(data)
           data = data.results;
+          console.log(data)
           res.render('unsplash', {data: data});
     
     }
@@ -25,9 +28,9 @@ routes.post('/', async (req, res)=>{
   const search = req.body.text;
 
   async function searchImages() {
-      const response = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${search}&client_id=${process.env.API_KEY}`
+      const response = await axios(`https://api.unsplash.com/search/photos?page=1&query=${search}&client_id=${process.env.API_KEY}`
       );
-      var data = await response.json();
+      var data = await response.data;
       data = data.results;
       res.render('unsplash', {data: data});
    }
